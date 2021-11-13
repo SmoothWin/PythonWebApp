26e54b01da76e4b1c418ea01697dffa327f86e92
Christian Chitanu <christian.chitanu@hotmail.com>
2021-11-13 15:52:30 -0500

    added test query for class

--- DAL/DBData.py
+++ DAL/DBData.py
@@ -1,4 +1,4 @@
-import psycopg2
+from psycopg2 import connect, Error
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
@@ -6,13 +6,24 @@ load_dotenv()
 class DBData:
     def __init__(self):
         try:
-            self.con = psycopg2.connect(host = dotenv_values("../.env.DB_HOST"),
-                                        dbname = dotenv_values("../.env.DB_DATABASE"),
-                                        user = dotenv_values("../.env.DB_USER"),
-                                        port = dotenv_values("../.env.DB_PORT"),
-                                        password = dotenv_values("../.env.DB_PASSWORD"))
-
-        except psycopg2.Error as e:
+            self.con = connect(host=dotenv_values("../.env.DB_HOST"),
+                                        dbname=dotenv_values("../.env.DB_DATABASE"),
+                                        user=dotenv_values("../.env.DB_USER"),
+                                        port=dotenv_values("../.env.DB_PORT"),
+                                        password=dotenv_values("../.env.DB_PASSWORD"))
+        except Error as e:
             print(e)
             self.con.close()
+
     def select_query(self, table_name, params=None):
+        return_set = []
+        cursor = self.con.cursor()
+        try:
+            query = 'SELECT * FROM (%s)'
+            cursor.execute(query=query, params=table_name)
+            return cursor.fetchall()
+        except Error as e:
+            print(e)
+            self.con.close()
+
+

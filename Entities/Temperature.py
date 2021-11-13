e18006a000f372127c40cdc3ca7bd72f7ef1c365
Christian Chitanu <christian.chitanu@hotmail.com>
2021-11-13 14:48:20 -0500

    added a to dictionary method for Temperature.py (good for transforming for responses)

--- Entities/Temperature.py
+++ Entities/Temperature.py
@@ -1,7 +1,13 @@
 class Temperature:
     date_time = None
     temperature = 0
+
     def __init__(self, date_time, temperature):
         self.date_time = date_time
         self.temperature = temperature
 
+    def to_dict(self):
+        return{
+            "date_time": self.date_time,
+            "temperature": self.temperature
+        }

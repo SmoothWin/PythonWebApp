3be20eef20011bffa32fd733aaa134b64adf43a3
Christian Chitanu <christian.chitanu@hotmail.com>
2021-11-13 14:46:30 -0500

    added Temperature entity

---
+++ Entities/Temperature.py
@@ -0,0 +1,7 @@
+class Temperature:
+    date_time = None
+    temperature = 0
+    def __init__(self, date_time, temperature):
+        self.date_time = date_time
+        self.temperature = temperature
+

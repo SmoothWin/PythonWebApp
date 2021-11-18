ea9d3cd9a062b5ab01dbdf652c02f34864ef5a06
Christian Chitanu <christian.chitanu@hotmail.com>
2021-11-18 12:06:59 -0500

    fixed the cursed problem

--- generator.py
+++ generator.py
@@ -5,13 +5,39 @@ from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 import os
 
-dicti = {'temperature': [{'date_time': 'Sat, 13 Nov 2021 19:38:29 GMT', 'temperature': 12}], 'humidity': [{'date_time': 'Sat, 13 Nov 2021 19:38:29 GMT', 'humidity': 0.23}], 'status': [{'date_time': 'Sat, 13 Nov 2021 19:38:29 GMT', 'online': True}]}
+dicti = {
+  "temperature": [
+    {
+      "date_time": "2021-11-17 22:21:36",
+      "temperature": 26.0
+    }
+  ],
+  "humidity": [
+    {
+      "date_time": "2021-11-17 22:21:36",
+      "humidity": 0.42
+    }
+  ],
+  "status": [
+    {
+      "date_time": "2021-11-17 22:21:36",
+      "online": 1
+    }
+  ]
+}
+
 
 load_dotenv()
 f = Fernet(os.environ.get('REQUEST_SECRET'))
 key = Fernet.generate_key()
 print(list(dicti.values()))
+string = ""
+print(dicti.items())
+for i in dicti.keys():
+  string += str(dicti[i])
+print(string);
 token = f.encrypt(bytes(str(list(dicti.values())), 'ASCII'))
 print("Token: {}".format(token))
+print(f.decrypt(bytes('gAAAAABhldLPYQshYou9l7c-rjGIc2aG8-zv9I5P-GgCCDxh6Q-paYleJC4Bf89QdWpa2R7s9val9v4CA0fpVTyRd_XwCcP9CsSz4SJRyYFOZ9w1-nGG0yUznYvMNeaad0b3037PVQRljW1Hg8XhDQdd1Gy74Qv4jc5kPl7ww17L8vm-wHEBgy0G7GSUP-Um0uZm-E1MaSvVovCynzLz4qW0oubxsCMg5xefZ12kqcoBDdQ488cetWL6i6OD-MVMUwxco8pIc778OOvVcuGYi8yOBXg4iOdeMe2rbCi9D7z2zQNaMjk6q0k=','ascii')))
 
 

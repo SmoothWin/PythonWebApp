2339316e28cd098d9a73b67bc5b7a79089ca24da
Christian Chitanu <christian.chitanu@hotmail.com>
2021-12-03 16:00:35 -0500

    setting expire time for cookie 2

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ users = DBUser()
 def response_create(message, code=200, delete_cookie = False):
     response_mod = make_response(message, code)
     if delete_cookie:
-        response_mod.set_cookie("auth", expires=0)
+        response_mod.set_cookie("auth", '', expires=0)
     return response_mod
 
 
